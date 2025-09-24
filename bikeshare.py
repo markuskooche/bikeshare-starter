797974da9d8f9f119f317bc6e817ce521529e399
Markus Koch <markuskoch.social@icloud.com>
2025-09-24 19:15:47 +0200

    docs: add missing args in docblock

--- bikeshare.py
+++ bikeshare.py
@@ -48,7 +48,12 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """
+    Displays statistics on the most frequent times of travel.
+
+    Args:
+        (DataFrame) df - Pandas DataFrame containing city data filtered by month and day
+    """
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
@@ -67,7 +72,12 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """
+    Displays statistics on the most popular stations and trip.
+
+    Args:
+        (DataFrame) df - Pandas DataFrame containing city data filtered by month and day
+    """
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
@@ -86,7 +96,12 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """
+    Displays statistics on the total and average trip duration.
+
+    Args:
+        (DataFrame) df - Pandas DataFrame containing city data filtered by month and day
+    """
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
@@ -102,7 +117,12 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """
+    Displays statistics on bikeshare users.
+
+    Args:
+        (DataFrame) df - Pandas DataFrame containing city data filtered by month and day
+    """
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
